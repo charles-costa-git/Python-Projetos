9327c36b94576eed81c20171a689d65dc72b86d1
Charles <charles30.it@gmail.com>
2025-11-22 14:57:45 -0300

    Update variaveis.py

--- variaveis.py
+++ variaveis.py
@@ -1,2 +1,27 @@
-nome_usuario = 'Charles'
-print(f'O seu nome é: {nome_usuario}')
+nome_usuario = 'Charles Costa'
+print(f'O meu nome é: {nome_usuario}')
+
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'Charles', 44
+estado = True
+
+# Função type()
+# print(type(nome_usuario))
+# print(type(media))
+# print(type(n1))
+# print(type(nome))
+# print(type(idade))
+# print(type(estado))
+# print(type(1+2j))
+
+#Função isinstance()
+# a = 10
+# b = 'Sol'
+# print(isinstance(a, int))
+# print(isinstance(b, (str, int)))
+
+a = 40
+c = 3
+r = a * c
+print(r)
